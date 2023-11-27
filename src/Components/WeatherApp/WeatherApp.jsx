cc54568491765af8e7dc7f50a9b0b12b6d09b4e3
Oluwaseun <144266502+Fooagsmart@users.noreply.github.com>
2023-11-28 00:30:14 +0100

    adding ApiKEY

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -10,11 +10,35 @@ import wind_icon from '../Assets/wind.png'
 import humidity_icon from '../Assets/humidity.png'
 
 const WeatherApp = () => {
+
+    let api_key ='88660bd17a340c76c1ee3eac23f0a15d';
+
+    const search = async () =>{
+        const element = document.getElementsByClassName("cityInput")
+        if(element[0].value==="")
+        {
+            return 0;
+        }
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+
+        let response = await fetch(url);
+        let data = await response.json();
+        const humidity = document.getElementsByClassName("humidity-percent")
+        const wind =document.getElementsByClassName("wind-rate")
+        const temperature =document.getElementsByClassName("weather-temp")
+        const location= document.getElementsByClassName("weather-location")
+
+        humidity[0].innerHTML = data.main.humidity+' %';
+        wind[0].innerHTML = data.wind.speed+' km/h';
+        temperature[0].innerHTML = data.main.temp+'°c';
+        location[0].innerHTML = data.name;
+    }
+
   return (
     <div className='container'>
       <div className='top-bar'>
         <input type='text' className='cityInput' placeholder='search' />
-        <div className="search-icon">
+        <div className="search-icon" onClick={()=>{search()}}>
             <img src={search_icon} alt='' />
         </div>
       </div>
@@ -34,7 +58,7 @@ const WeatherApp = () => {
         <div className="element">
             <img src={wind_icon} alt='' className='icon' />
             <div className="data">
-                <div className="humidity-percent">18km/h</div>
+                <div className="wind-rate">18km/h</div>
                 <div className="text">Wind Speed</div>
             </div>
         </div>
