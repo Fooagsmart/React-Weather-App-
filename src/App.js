b5a7bfa66756442ab643bab760c6eb98683bc1c0
Oluwaseun <144266502+Fooagsmart@users.noreply.github.com>
2023-11-28 04:20:12 +0100

    QUOTE ADDED

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { useEffect } from 'react';
 import './App.css';
 import WeatherApp from './Components/WeatherApp/WeatherApp';
 
 function App() {
+
+  useEffect(()=>{
+    alert('A change in the weather is sufficient to recreate the world and ourselves.')
+  })
   return (
     <div className="App">
       <WeatherApp />
